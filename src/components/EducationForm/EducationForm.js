b70f17c172a5e0d7f862d8d2e8907b59743be8a5
NateTewolde <tewolde.nate@gmail.com>
2022-11-16 01:38:40 -0800

    Reworked react class components into functional components

--- src/components/EducationForm/EducationForm.js
+++ src/components/EducationForm/EducationForm.js
@@ -1,41 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import uniqid from "uniqid";
 
-class EducationForm extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-  }
+const EducationForm = ({
+  eduNum,
+  eduSchoolEntry,
+  eduTitleEntry,
+  eduDateEntry,
+  handleChange,
+}) => {
+  return (
+    <div key={uniqid()}>
+      <label htmlFor="education">School:</label>
+      <input
+        onBlur={handleChange}
+        type="text"
+        id={`eduSchoolInput${eduNum}`}
+        defaultValue={eduSchoolEntry}
+      />
 
-  render() {
-    return (
-      <div key={uniqid()}>
-        <label htmlFor="education">School:</label>
-        <input
-          onBlur={this.props.handleChange}
-          type="text"
-          id={`eduSchoolInput${this.props.eduNum}`}
-          defaultValue={this.props.eduSchoolEntry}
-        />
+      <label htmlFor="education">Study:</label>
+      <input
+        onBlur={handleChange}
+        type="text"
+        id={`eduTitleInput${eduNum}`}
+        defaultValue={eduTitleEntry}
+      />
 
-        <label htmlFor="education">Study:</label>
-        <input
-          onBlur={this.props.handleChange}
-          type="text"
-          id={`eduTitleInput${this.props.eduNum}`}
-          defaultValue={this.props.eduTitleEntry}
-        />
-
-        <label htmlFor="education">Date:</label>
-        <input
-          onBlur={this.props.handleChange}
-          type="date"
-          id={`eduDateInput${this.props.eduNum}`}
-          defaultValue={this.props.eduDateEntry}
-        />
-      </div>
-    );
-  }
-}
+      <label htmlFor="education">Date:</label>
+      <input
+        onBlur={handleChange}
+        type="date"
+        id={`eduDateInput${eduNum}`}
+        defaultValue={eduDateEntry}
+      />
+    </div>
+  );
+};
 
 export default EducationForm;
